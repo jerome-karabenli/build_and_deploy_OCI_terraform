11aaac32d901a022bd834c7f9edd3c49d6f60c9a
Jerome Karabenli <jkarabenli.dev@gmail.com>
2022-03-03 12:39:13 +0100

    test config

--- output.tf
+++ output.tf
@@ -3,11 +3,6 @@ output "master_instance_ip" {
   value = oci_core_instance.ubuntu_1.public_ip
 }
 
-# Adresse IP de la vm crée
-output "backup_instance_ip" {
-  value = oci_core_instance.ubuntu_2.public_ip
-}
-
 # Votre adresse IP public
 output "my_ip_addr" {
   value = local.ifconfig_co_json.ip
